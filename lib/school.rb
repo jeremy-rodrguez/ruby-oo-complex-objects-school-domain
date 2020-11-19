c1c068d296f2f7a8eccaced5037b8806d5c15fe8
Jeremy <jeremy.rodrguez@yahoo.com>
2020-11-18 20:18:40 -0800

    Dustin and Jeremy's Doing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+    attr_accessor :roster
+    def initialize(school)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade) # AC Slater, 9
+        if @roster[grade]
+            @roster[grade] << student
+        else
+            @roster[grade] = [] << student
+        end
+    end
+
+    def grade(grade_level)
+        roster[grade_level]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+    end
+end
